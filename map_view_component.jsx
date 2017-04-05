8ee687d37737273bb544642cfcacacb02609a920
beebeean09 <beebeean09@gmail.com>
2017-04-04 17:59:26 -0700

    add map view component class

---
+++ map_view_component.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import MapView from 'react-native-maps';
+import {
+  View,
+  Text
+} from 'react-native';
+
+
+class Map extends Component {
+  
+}
+
+export default Map;
