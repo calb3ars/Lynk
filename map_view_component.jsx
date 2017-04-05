e261699234362cb16b4a2ed70d1a3ac041c1eb3b
beebeean09 <beebeean09@gmail.com>
2017-04-04 18:03:40 -0700

    add stylesheet for map

--- map_view_component.jsx
+++ map_view_component.jsx
@@ -7,7 +7,28 @@ import {
 
 
 class Map extends Component {
-  
+  render() {
+    return (
+      <View style={styles.container}>
+        <MapView>
+
+        </MapView>
+      </View>
+    );
+  }
 }
 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  map: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+  }
+});
 export default Map;
