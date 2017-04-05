42e68340b595531c0290880184064d07a759572f
beebeean09 <beebeean09@gmail.com>
2017-04-04 21:59:39 -0700

    install react native maps

--- map_view_component.jsx
+++ map_view_component.jsx
@@ -10,8 +10,16 @@ class Map extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <MapView>
-
+        <MapView
+          style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          mapType='standard'
+          showsUserLocation={true}>
         </MapView>
       </View>
     );
