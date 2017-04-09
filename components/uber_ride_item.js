23edabb02cf7047cb1abad7d84b48b421f2912c7
Tony Xie <tony_xie@outlook.com>
2017-04-09 14:04:38 -0700

    redirect and pass location data

--- components/uber_ride_item.js
+++ components/uber_ride_item.js
@@ -64,7 +64,6 @@ export default class UberRideItem extends Component {
         <View style={styles.uberListing, styles.listing}>
           <Text style={[styles.uberRideType, styles.type]}>{this.props.ride.display_name}</Text>
           <Text style={[styles.uberCost, styles.cost]}><Text style={styles.dollar}>$</Text>{this.props.ride.high_estimate}</Text>
-          <Text style={[styles.uberPrimeTime, styles.bonus]}>Surge: {this.props.ride.primetime_percentage}</Text>
           <Text style={[styles.uberRideTime, styles.time]}>Ride Time: {Math.floor(this.props.ride.duration / 60)} min</Text>
         </View>
       </TouchableHighlight>
