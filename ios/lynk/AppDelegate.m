abbba1eef60781b48d5ab90f6361553771b5a3d6
Tony Xie <tony_xie@outlook.com>
2017-04-09 18:46:45 -0700

    fix error codes

--- ios/lynk/AppDelegate.m
+++ ios/lynk/AppDelegate.m
@@ -18,8 +18,9 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-
+  // jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+  jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"lynk"
                                                initialProperties:nil
